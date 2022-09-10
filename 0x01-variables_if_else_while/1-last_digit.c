d6d6b24042bbb5bfe9f967df8d22c1b4082147ed
samomunene <samomunene@gmail.com>
2022-09-09 19:34:10 -0700

    number one

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - print if the number is positive, zero, or negative
+ * main - Entry point
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative"
@@ -12,22 +12,23 @@ int main(void)
 {
 int n;
 int l;
+char str[] = "last digit of";
 
-srand (time(0));
+srand(time(0));
 n = rand() - RAND_MAX / 2;
 l = n % 10;
 
 if (l > 5)
 {
-	printf("Last digit of %d is %d and is greater than 5\n, n, l);
+	printf("Last digit of %d is %d and is greater than 5\n, str, n, l);
 }
 else if (l ==0)
 {
-	printf("Last digit of %d is %d and is 0\n", n, l);
+	printf("Last digit of %d is %d and is 0\n", str, n, l);
 }
-else
+else if (l < 6)
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", str, n, l);
 }
 return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
  * main - print if the number is positive, zero, or negative
  *
  * Description: using the main function
- * this program prints "0,1,2,3,4,5,6,7,8,9"
+ * this program prints "0 1 2 3 4 5 6 7 8 9"
  * return: 0
  */
 int main(void)
