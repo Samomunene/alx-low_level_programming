7680957bd6a73ff753e7aa49081b7052271fda3e
samomunene <samomunene@gmail.com>
2022-09-09 17:09:08 -0700

    upper case lower

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,11 @@ int main(void)
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-for (ch = 'A'; ch <='Z'; ch++)
-{
 	 putchar(ch);
 }
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+	putchar(ch);
 }
 putchar('\n');
 return (0);
