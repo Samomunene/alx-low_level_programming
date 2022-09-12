95fd5a815e409d12dbb94193affc8e32e3adcf96
samomunene <samomunene@gmail.com>
2022-09-12 13:17:15 -0700

    eight

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - print if the number is positive, zero, or negative
  *
@@ -9,11 +9,11 @@
 int main(void)
 {
 char c;
-for (c = '0'; c <='9'; c++)
+for (c = '0'; c <= '9'; c++)
 {
 	putchar(c);
 }
-for (c = 'a'; c <='f'; c++)
+for (c = 'a'; c <= 'f'; c++)
 {
 	putchar(c);
 }
