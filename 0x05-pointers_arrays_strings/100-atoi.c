3d5b6a3ceabfdf190c0b6b1dfa8f7cc55a8cc989
Samomunene <samomunene@gmail.com>
2022-09-19 16:44:01 -0700

    password generator

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,9 +13,9 @@ int _atoi(char *s)
 	unsigned int num = 0;
 
 	do {
-		if  (*s == '-')
+		if (*s == '-')
 			sign *= -1;
-		else if (*s >= '0' && *s <+ '9')
+		else if (*s >= '0' && *s <= '9')
 			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
