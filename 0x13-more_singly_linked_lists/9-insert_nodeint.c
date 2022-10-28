60b01b0464bf6ccb9518671ba71a89934ce62b3c
Samomunene <samomunene@gmail.com>
2022-10-27 17:13:32 -0700

    inserting nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,6 +39,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = h->next;
 		h->next = new;
 	}
-
+	{
+		new->next = h->next;
+		h->next = new;
+	}
 	return (new);
 }
