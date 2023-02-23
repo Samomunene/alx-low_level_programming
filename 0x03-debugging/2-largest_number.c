f3c490c66ca67f941a0249bbb9afa86de7c0b13f
Samomunene <samomunene@gmail.com>
2023-02-23 15:23:46 -0800

    makefile

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -8,16 +9,14 @@
  * Return: largest number
  */
 
-int larges_number(int a, int b, int c)
-{
-	int largest;
+int largest_number(int a, int b, int c)
+{int largest;
 
-	if (a >= b && a >= c)
-
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b >= a && b >= c)
+	else if ( b > c)
 	{
 		largest = b;
 	}
